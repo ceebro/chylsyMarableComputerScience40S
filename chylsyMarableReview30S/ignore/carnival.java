5a0fed4185d25c32a454b6bb3c1b11a6ad076cdf
chylsy <128323856+ceebro@users.noreply.github.com>
2023-09-07 11:51:55 -0500

    Update carnival.java

--- chylsyMarableReview30S/ignore/carnival.java
+++ chylsyMarableReview30S/ignore/carnival.java
@@ -1,8 +1,28 @@
-public class Carnival{
 
-  public static void main(String[] args){
+public class Carnival
+{
+    
+    public static final String TITLE = "The Carnival";
+    
+    public static void main(String args[]) {
 
-    System.out.println("Hello Carnival");
-
-  }
+        welcome();
+        
+    }
+    
+    public static void welcome(){
+        
+        String welcomeMessage = "Welcome to " + TITLE + "!";
+        output(welcomeMessage);
+    }
+    
+    public static void output(String message){
+        
+        JOptionPane.showMessageDialog(
+            null,
+            message,
+            TITLE,
+            JOptionPane.PLAIN_MESSAGE
+        );
+    }
 }
