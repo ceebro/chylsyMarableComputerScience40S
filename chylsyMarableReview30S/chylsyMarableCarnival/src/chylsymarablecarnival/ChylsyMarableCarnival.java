756e042b13d103fea734281e665c5942b25091ee
chylsy <128323856+ceebro@users.noreply.github.com>
2023-09-20 11:58:30 -0500

    Update ChylsyMarableCarnival.java

--- chylsyMarableReview30S/chylsyMarableCarnival/src/chylsymarablecarnival/ChylsyMarableCarnival.java
+++ chylsyMarableReview30S/chylsyMarableCarnival/src/chylsymarablecarnival/ChylsyMarableCarnival.java
@@ -29,6 +29,12 @@ public class ChylsyMarableCarnival {
         output(welcomeMessage);
     }
     
+    public static void thanks(){
+        
+        String thanksMessage = "Thank you for using " + TITLE + "!";
+        output(thanksMessage);
+    }
+    
     public static void program(){
         
         String[] activities = new String[4];
@@ -40,10 +46,39 @@ public class ChylsyMarableCarnival {
         String activityMessage = "Welcome to the " + TITLE + ", what would you like to do?";
         
         Object userActivity = userInput(activityMessage, activities);
+
+        Balloon balloon = new Balloon();
+        String balloonName = balloon.setName().toString();
+        String balloonColor = balloon.setColor().toString();
+
+        String[] directions = balloon.setDirections();
+
+        String userBalloon = "Name: " + balloonName + "\n"
+                + "Color: " + balloonColor + "\n"
+                + "Floating: " + directions[0];
+        output(userBalloon);
         
-        if(userActivity == activities[0]){
+        userActivity = userInput(activityMessage, activities);
+        
+        if (userActivity == activities[1]){
             
+            String direction = balloon.moveBalloon().toString();
             
+            userBalloon = "Name: " + balloonName + "\n"
+                + "Color: " + balloonColor + "\n"
+                + "Floating: " + direction;
+            output(userBalloon);
+            
+            userActivity = userInput(activityMessage, activities);
+        } else if(userActivity == activities[2]){
+            
+            String poppedBalloon = "Your balloon " + balloonName + " has popped!";
+            output(poppedBalloon);
+            
+            userActivity = userInput(activityMessage, activities);
+        } else if(userActivity == activities[3]){
+            
+            thanks();
         }
     }
     
@@ -61,7 +96,8 @@ public class ChylsyMarableCarnival {
     
     public static void output(String message){
         
-        JOptionPane.showMessageDialog(null, 
+        JOptionPane.showMessageDialog(
+                null, 
                 message, 
                 TITLE, 
                 JOptionPane.PLAIN_MESSAGE,
