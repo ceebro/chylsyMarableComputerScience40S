8d59e8a7a71d87a9bcd89937095974f0d73298ea
chylsy <128323856+ceebro@users.noreply.github.com>
2023-09-22 12:30:06 -0500

    Update ChylsyMarableCarnival.java

--- chylsyMarableReview30S/chylsyMarableCarnival/src/chylsymarablecarnival/ChylsyMarableCarnival.java
+++ chylsyMarableReview30S/chylsyMarableCarnival/src/chylsymarablecarnival/ChylsyMarableCarnival.java
@@ -3,14 +3,16 @@ package chylsymarablecarnival;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author c.marable
- */
+/*
+ * chylsyMarableCarnival - A program that simulates a carnival by creating
+    balloons for the user. They also have the option to move and pop their 
+    balloon.
+ * @author Chylsy Marable
+ * @since 6-Sep-2023
+*/
 public class ChylsyMarableCarnival {
 
     public static final String TITLE = "The Carnival";
-
     static ImageIcon balloons = new ImageIcon("balloons.gif");
 
     /**
@@ -21,19 +23,29 @@ public class ChylsyMarableCarnival {
         welcome();
         program();
     }
-
+    
+    /**
+     * Outputs user welcome message
+     */
     public static void welcome() {
 
         String welcomeMessage = "Welcome to " + TITLE + "!";
         output(welcomeMessage);
     }
 
+    /**
+     * Outputs user thank you message
+     */
     public static void thanks() {
 
         String thanksMessage = "Thank you for using " + TITLE + "!";
         output(thanksMessage);
     }
 
+    /**
+     * Executes the program by creating a balloon and asking the user for their
+     * activity choice
+     */
     public static void program() {
 
         String[] activities = new String[4];
@@ -46,32 +58,38 @@ public class ChylsyMarableCarnival {
 
         Object userActivity = userInput(activityMessage, activities);
 
-        Balloon gavin = new Balloon();
-        gavin.name = Balloon.setName();
-        gavin.color = Balloon.setColor();
-        String[] directions = gavin.setDirections();
+        Balloon balloon = new Balloon();
+        balloon.name = Balloon.setName();
+        balloon.color = Balloon.setColor();
+        String[] directions = balloon.setDirections();
 
-        gavin.outputProperties(directions[0]);
+        balloon.outputProperties(directions[0]);
 
         userActivity = userInput(activityMessage, activities);
+        
         do {
             if(userActivity == activities[1]) {
 
-                String direction = gavin.moveBalloon();
-                gavin.outputProperties(direction);
+                String direction = balloon.moveBalloon();
+                balloon.outputProperties(direction);
 
                 userActivity = userInput(activityMessage, activities);
             } else if (userActivity == activities[2]) {
 
-                gavin.popBalloon();
+                balloon.popBalloon();
                 userActivity = userInput(activityMessage, activities);
             }
-
         } while (userActivity != activities[3]);
         
         thanks();
     }
 
+    /**
+     * Outputs a JOptionPane drop down dialog box and the user selects their choice
+     * @param message - The message of the dialog box
+     * @param options - The user's options
+     * @return - Returns the user's selected option
+     */
     public static Object userInput(String message, String[] options) {
 
         return JOptionPane.showInputDialog(
@@ -84,6 +102,10 @@ public class ChylsyMarableCarnival {
                 options[0]);
     }
 
+    /**
+     * Outputs a JOptionPane message dialog box
+     * @param message - The message of the dialog box
+     */
     public static void output(String message) {
 
         JOptionPane.showMessageDialog(
