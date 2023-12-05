180e5f7737a785a484dfdc9bd38215a414f0a351
chylsy <128323856+ceebro@users.noreply.github.com>
2023-12-05 12:01:38 -0600

    Create Sale.java

---
+++ chylsyMarableAdvancedClasses/Sale.java
@@ -0,0 +1,56 @@
+/** Required package class namespace */
+package chylsymarableadvancedclassesprogram;
+
+ 
+/**
+ * Sale.java - A generic class that creates sold items as well as an order
+ * number (the price of the item can be set)
+ *
+ * @author Chylsy Marable
+ * @since Dec 5, 2023, 11:09:23 a.m.
+ */
+public class Sale <T> extends Order
+{
+
+    private T saleItem;
+    private double price;
+    
+    /**
+     * Default constructor, set class properties
+     */
+    public Sale(T saleItem, int orderNumber) {
+        
+        super.orderNumber = orderNumber;
+        this.saleItem = saleItem;
+    }
+
+    /**
+     * Sets the price of the saleItem
+     * @param price - The price as a double
+     */
+    @Override
+    public void setPrice(double price) {
+        
+        this.price = price;
+    }
+
+    /**
+     * Gets the price of the saleItem
+     * @return - Returns the price as a double
+     */
+    @Override
+    public double getPrice() {
+        
+        return price;
+    }
+    
+    /**
+     * Gets the order number of the saleItem
+     * @return - Returns the order number
+     */
+    public int getOrderNumber(){
+        
+        return orderNumber;
+    }
+    
+}
