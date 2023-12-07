163c408bc14f73c682888c84a6b599361429fb8e
chylsy <128323856+ceebro@users.noreply.github.com>
2023-12-07 11:52:17 -0600

    Update Dog.java

--- chylsyMarableAdvancedClasses/Dog.java
+++ chylsyMarableAdvancedClasses/Dog.java
@@ -3,7 +3,7 @@ package chylsymarableadvancedclassesprogram;
 
  
 /**
- * Dog.java - description
+ * Dog.java - This class implements Pet and inherits its methods. Dog is a pet
  *
  * @author Chylsy Marable
  * @since Dec 5, 2023, 11:09:40 a.m.
@@ -13,31 +13,51 @@ public class Dog implements Pet
 
     private String name;
     private Sale sale;
-    
-    /**
-     * Default constructor, set class properties
-     */
-    public Dog() {
-        
-        
-    }
 
+    /**
+     * Overridden inherited method that sets the dog's name (mutator)
+     * @param name - The dog's name
+     */
     @Override
     public void setName(String name) {
         
         this.name = name;
     }
 
+    /**
+     * Accesses the dog's name
+     * @return - Returns the pet's name
+     */
     @Override
     public String getName() {
         
         return name;
     }
+    
+    /**
+     * Sells a dog (the Sale)
+     * @param price - The price of the sold item
+     * @param orderNumber - The Sale's order number
+     */
+    public void sell(double price, int orderNumber){
+        
+        sale = new Sale(this, orderNumber);
+        sale.setPrice(price);
+    }
 
+    /**
+     * Outputs the order details on the console
+     */
     @Override
     public void sold() {
         
-        
+        System.out.print(
+                "Order " 
+                + sale.getOrderNumber() + " "
+                + name
+                + " is worth " 
+                + sale.getPrice()
+        );
     }
      
    
