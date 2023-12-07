9b168e4de9fec2dd34f19c161a6c0b339da071f5
chylsy <128323856+ceebro@users.noreply.github.com>
2023-12-07 11:53:37 -0600

    Update Pet.java

--- chylsyMarableAdvancedClasses/Pet.java
+++ chylsyMarableAdvancedClasses/Pet.java
@@ -2,8 +2,8 @@
 package chylsymarableadvancedclassesprogram;
 
 /**
- * Pet.java - Pet interface that acts as a set of rules for Pet 
- * objects (Dog, Poodle)
+ * Pet.java - A Pet interface that acts as a set of rules for Pet 
+ * objects (Dog, Poodle (classes that implement Pet))
  *
  * @author Chylsy Marable
  * @since Dec 5, 2023, 11:09:02 a.m.
@@ -11,13 +11,13 @@ package chylsymarableadvancedclassesprogram;
 public interface Pet {
     
     /**
-     * Sets the pet's name
+     * Sets the pet's name (mutator)
      * @param name - Pet's name
      */
     public void setName(String name);
     
     /**
-     * Gets the pet's name
+     * Gets the pet's name (accessor method)
      * @return - Returns the pet's name (String)
      */
     public String getName();
