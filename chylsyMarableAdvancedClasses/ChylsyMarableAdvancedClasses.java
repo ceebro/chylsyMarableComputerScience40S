bf521047c958b5e7d2f12760f8513fe42514da0e
chylsy <128323856+ceebro@users.noreply.github.com>
2023-12-07 11:52:05 -0600

    Update ChylsyMarableAdvancedClasses.java

--- chylsyMarableAdvancedClasses/ChylsyMarableAdvancedClasses.java
+++ chylsyMarableAdvancedClasses/ChylsyMarableAdvancedClasses.java
@@ -1,1 +1,26 @@
 
+/** Required package class namespace */
+package chylsymarableadvancedclassesprogram;
+
+
+/*
+ * ChylsyMarableAdvancedClassesProgram - This programming problem simulates
+ * a digital pet shop where you sell poodles.
+ * 
+ * @author Chylsy Marable
+ * @since Dec 5, 2023
+*/
+public class ChylsyMarableAdvancedClassesProgram
+{
+
+    /**
+     * Main method for the project
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Poodle daniel = new Poodle("Daniel Jun", 101);
+        Poodle ryland = new Poodle("Ryland Cameron Tsuyuki", 102);
+    }
+}
